6d4b4cc4a0c2a63b9e038ff9e2791bdd91ee33a0
Yohaan Chokhany <yohaanchokhany@gmail.com>
2024-01-20 20:43:21 -0500

    Add new user info route and update port number

--- main.py
+++ main.py
@@ -106,10 +106,15 @@ def logout():
     )
 
 
+@app.route("/new-user-info/")
+def new_user_info():
+    return render_template("new-user-info.html")
+
+
 @app.route("/settings/")
 def settings():
     return render_template("settings.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(env.get("PORT", 3000)))
